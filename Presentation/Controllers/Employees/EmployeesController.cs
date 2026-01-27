0a5dc6630e3536d0e69e981cdc8e0d9d70f60153
Toan <tktoan10a1@gmail.com>
2026-01-27 16:14:33 +0700

    feat: Add get all employees

---
+++ Presentation/Controllers/Employees/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using FoodHub.Application.Extensions.Pagination;
+using FoodHub.Application.Features.Employees.Queries.GetEmployees;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodHub.Presentation.Controllers.Employees
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public EmployeesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetEmployeesAsync([FromQuery] PaginationParams pagination)
+        {
+            var query = new GetEmployees.Query(pagination);
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
