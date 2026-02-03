d8b50df906fed476e3dc9e6adeddf97337de1afb
Toan <tktoan10a1@gmail.com>
2026-02-03 16:00:12 +0700

    feat: Add JwtSettings class to define JWT configuration properties.

--- Infrastructure/Security/JwtSettings.cs
+++ Infrastructure/Security/JwtSettings.cs
@@ -5,7 +5,7 @@
         public string SecretKey { get; set; } = null!;
         public string Issuer { get; set; } = null!;
         public string Audience { get; set; } = null!;
-        public int ExpiresInMinute { get; set; }
-        public int RefreshTokenExpiresInDays { get; set; }
+        public int ExpiresInMinute { get; set; } = 60; // 1 hour default
+        public int RefreshTokenExpiresInDays { get; set; } = 7; // 7 days default
     }
 }
