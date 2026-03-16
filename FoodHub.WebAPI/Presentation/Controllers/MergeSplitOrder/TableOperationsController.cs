d17a40aa9a4ab45e8f00d24d69aef2ab991684ac
Toan <tktoan10a1@gmail.com>
2026-03-16 16:57:15 +0700

    feat: add API endpoints for changing, merging, and splitting orders via `TableOperationsController`.

--- FoodHub.WebAPI/Presentation/Controllers/MergeSplitOrder/TableOperationsController.cs
+++ FoodHub.WebAPI/Presentation/Controllers/MergeSplitOrder/TableOperationsController.cs
@@ -22,9 +22,7 @@ namespace FoodHub.WebAPI.Presentation.Controllers.MergeSplitOrder
             _mediator = mediator;
         }
 
-        /// <summary>
-        /// Đổi bàn cho một đơn hàng (Lúc này đơn hàng & món phải được mang sang bàn mới).
-        /// </summary>
+        /// <summary>Đổi bàn cho một đơn hàng. Món ăn và thông tin đơn được chuyển sang bàn mới.</summary>
         [HttpPatch("{id:guid}/change-table")]
         [HasPermission(Permissions.Orders.ChangeTable)] // Hoặc cần quyền chuyên biệt
         [RateLimit(maxRequests: 50, windowMinutes: 1, blockMinutes: 5)]
@@ -40,9 +38,7 @@ namespace FoodHub.WebAPI.Presentation.Controllers.MergeSplitOrder
             return HandleResult(result);
         }
 
-        /// <summary>
-        /// Gộp hai đơn hàng thành một. Khách hàng từ đơn này được gom vào đơn kia.
-        /// </summary>
+        /// <summary>Gộp hai đơn hàng thành một. Khách hàng và món được gom sang đơn đích.</summary>
         [HttpPost("{id:guid}/merge")]
         [HasPermission(Permissions.Orders.Merge)] // Hoặc cần quyền chuyên biệt
         [RateLimit(maxRequests: 50, windowMinutes: 1, blockMinutes: 5)]
@@ -58,9 +54,7 @@ namespace FoodHub.WebAPI.Presentation.Controllers.MergeSplitOrder
             return HandleResult(result);
         }
 
-        /// <summary>
-        /// Tách một đơn hàng thành hai. Một phần món ăn được tách ra thành đơn mới.
-        /// </summary>
+        /// <summary>Tách một đơn hàng thành hai. Một phần món được chuyển sang đơn mới.</summary>
         [HttpPost("{id:guid}/split")]
         [HasPermission(Permissions.Orders.Split)] // Hoặc cần quyền chuyên biệt
         [RateLimit(maxRequests: 50, windowMinutes: 1, blockMinutes: 5)]
