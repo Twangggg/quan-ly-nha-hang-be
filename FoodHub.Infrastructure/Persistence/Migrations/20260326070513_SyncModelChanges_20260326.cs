6e79f420551dce2f248599613904b9b7307c0f4a
Toan <tktoan10a1@gmail.com>
2026-03-26 14:15:08 +0700

    feat: add database migration to synchronize model changes.

---
+++ FoodHub.Infrastructure/Persistence/Migrations/20260326070513_SyncModelChanges_20260326.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncModelChanges_20260326 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "grace_period_minutes",
+                table: "reservation_settings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 15,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "grace_period_minutes",
+                table: "reservation_settings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldDefaultValue: 15);
+        }
+    }
+}
